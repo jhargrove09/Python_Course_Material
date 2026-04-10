eb90eef3d0afa0207eddaeb2f1a33f01a8aea34f
jhargrove09 <j.hargrove09@gmail.com>
2026-04-09 22:06:54 -0400

    create base

---
+++ week3b.py
@@ -0,0 +1,10 @@
+#menu 1. Add Student 2. View Students 3. Exit
+int(input("Enter your choice: "))
+input("Enter student name: ")
+input("Enter student score: ")
+#when finished 
+print("Student has been added.")
+
+#When choice 2 selected print student data
+#when choice 3 selected
+print("Data saved. Thank you, have a great day!")
